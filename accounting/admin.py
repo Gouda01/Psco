3f4955a37e279c40bf726724e55b11fb9f7f2d77
Mohamed Gouda <goda61@gmail.com>
2025-07-24 18:17:22 +0300

    Work in treasury

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Cashbox, CashTransaction
+
+
 # Register your models here.
+admin.site.register(Cashbox)
+admin.site.register(CashTransaction)
--- accounting/models.py
+++ accounting/models.py
@@ -8,14 +8,15 @@ User = get_user_model()
 
 
 class Cashbox(models.Model):
-    name = models.CharField(max_length=100, default="الخزينة الرئيسية")
+    name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    is_main = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.name} - {self.branch.name}"
+        return f"{self.name}"
 
 
 class CashTransaction(models.Model):
